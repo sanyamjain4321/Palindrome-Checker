0a10bcac19a742996445858663f927fbc83b390a
sanyamjain4321 <ks2578@srmist.edu.in>
2026-02-25 09:36:40 +0530

    UC4:Character Array Base

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,29 +3,37 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Take input from user
-        System.out.print("Enter a string: ");
+        // Take input
+        System.out.print(" Input: ");
         String input = scanner.nextLine();
 
-        // Variable to store reversed string
-        String reversed = "";
+        // Convert string to character array
+        char[] characters = input.toCharArray();
 
-        // Iterate from last character to first
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        // Two-pointer technique
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
-        // Display reversed string
-        System.out.println("Reversed string: " + reversed);
-
-        // Compare original and reversed string
-        if (input.equals(reversed)) {
-            System.out.println("Result: The string is a PALINDROME.");
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Is Palindrome? : true");
         } else {
-            System.out.println("Result: The string is NOT a palindrome.");
+            System.out.println("Is Palindrome? : false");
         }
 
-        scanner.close();
-    }
+        scanner.close();    }
 }
 
